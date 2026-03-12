58a1c8f693f90d3013cb3499c933727655a4e15d
Vodocap <70716792+Vodocap@users.noreply.github.com>
2026-03-12 15:55:34 +0000

    Deploying to gh-pages from @ Vodocap/diplomka@9c01ede611dca2964ae24fc0b57dde3a1915d2b3 🚀

--- pkg/wasm.js
+++ pkg/wasm.js
@@ -1032,7 +1032,7 @@ function __wbg_get_imports() {
             console.warn(arg0);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 472, function: Function { arguments: [Externref], shim_idx: 473, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 476, function: Function { arguments: [Externref], shim_idx: 477, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__he19b5b7ba5d0617d, wasm_bindgen__convert__closures_____invoke__hfd7798dac3d4e96a);
             return ret;
         },
